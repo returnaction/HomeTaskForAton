13ef60f7366e40c2432ad6963d6e517a9f6ca16b
returnaction <obergannv@gmail.com>
2024-06-18 14:59:32 -0400

    Add client class and Enums

---
+++ HomeTaskForAton/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeTaskForAton.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public int AccountNumber { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; }
+        public DateTime DOB { get; set; }
+        public long INN { get; set; }
+        public string ResponsiblePerson { get; set; }
+
+        public Status Status { get; set; } = Status.Authorized;
+
+    }
+
+    public enum Status
+    {
+        Unauthorized,
+        Authorized,
+        Canceled,
+        Closed
+    }
+}
